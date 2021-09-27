347af613ecb3cef4f567395664873bc6c27afbc3
vova4 <435335vova@gmail.com>
2021-09-27 16:08:44 +0300

    Инициализация AppComponent

--- app/src/main/java/ru/vld43/mangaapp/ui/MainViewModel.kt
+++ app/src/main/java/ru/vld43/mangaapp/ui/MainViewModel.kt
@@ -2,6 +2,11 @@ package ru.vld43.mangaapp.ui
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import ru.vld43.mangaapp.App
 
 class MainViewModel(application: Application): AndroidViewModel(application) {
+
+    init {
+        App.appComponent.inject(this)
+    }
 }
