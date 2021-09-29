5615cec33b3bdc51d9d418267edca08f2210c682
vova4 <435335vova@gmail.com>
2021-09-29 23:54:54 +0300

    Вызов методов repository в MainViewModel

--- app/src/main/java/ru/vld43/mangaapp/ui/main/MainViewModel.kt
+++ app/src/main/java/ru/vld43/mangaapp/ui/main/MainViewModel.kt
@@ -1,12 +1,45 @@
 package ru.vld43.mangaapp.ui.main
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import ru.vld43.mangaapp.App
+import ru.vld43.mangaapp.data.MangaRepository
+import ru.vld43.mangaapp.domain.Manga
+import javax.inject.Inject
 
-class MainViewModel(application: Application): AndroidViewModel(application) {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    @Inject
+    lateinit var mangaRepository: MangaRepository
+
+    private val disposables = CompositeDisposable()
+    val liveData = MutableLiveData<List<Manga>>()
 
     init {
         App.appComponent.inject(this)
+        loadManga()
+    }
+
+    override fun onCleared() {
+        disposables.dispose()
+        super.onCleared()
+    }
+
+    fun loadManga() {
+        disposables.add(
+            mangaRepository.getMangaList()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    liveData.value = it
+                }, {
+                    Log.i("MainViewModel", "$it")
+                })
+        )
     }
 }
