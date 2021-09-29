e933761eda024ebee83cd22c85b5626740959bd3
vova4 <435335vova@gmail.com>
2021-09-29 16:24:42 +0300

    Создание ApiConstants

---
+++ app/src/main/java/ru/vld43/mangaapp/data/ApiConstants.kt
@@ -0,0 +1,12 @@
+package ru.vld43.mangaapp.data
+
+object ApiConstants {
+
+    const val BASE_URL = "https://api.mangadex.org"
+    const val COVER_URL = "https://uploads.mangadex.org/covers/"
+
+    const val GET_MANGA_LIST = "manga"
+    const val GET_VIEW_MANGA = "manga/"
+    const val GET_COVER_ART_LIST = "cover"
+    const val GET_COVER = "cover/"
+}
